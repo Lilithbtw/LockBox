87f39b48b972793f1b5084b2407a999a82f0088b
Lilithbtw <lili.macias@proton.me>
2026-03-03 19:05:20 +0100

    changed for any option as allowed hosts

--- app/middleware.py
+++ app/middleware.py
@@ -10,11 +10,13 @@ load_dotenv("../.env")
 
 SESSION_SECRET = secrets.token_urlsafe(32)
 
-ALLOWED_HOSTS_STR = os.getenv("ALLOWED_HOSTS", "localhost")
-ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS_STR.split(",") if host.strip()]
-
 middleware = [
-    Middleware(TrustedHostMiddleware, allowed_hosts=ALLOWED_HOSTS),
     Middleware(GZipMiddleware, minimum_size=1000, compresslevel=9),
     Middleware(SessionMiddleware, secret_key=SESSION_SECRET),
 ]
+
+ALLOWED_HOSTS_STR = os.getenv("ALLOWED_HOSTS", "any")
+
+if ALLOWED_HOSTS_STR.lower() != "any":
+    ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS_STR.split(",") if host.strip()]
+    middleware.append(Middleware(TrustedHostMiddleware, allowed_hosts=ALLOWED_HOSTS))
