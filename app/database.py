9b8fff3d8d208d035a5c2e1698317954d818e700
Lilithbtw <lili.macias@proton.me>
2026-03-03 22:50:09 +0100

    added database retry loop

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import OperationalError
 from os import getenv
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
@@ -17,10 +19,19 @@ DATABASE_URL = f"{SERVER_URL}/{DB_NAME}"
 
 async def create_db_if_not_exists():
     temp_engine = create_async_engine(SERVER_URL)
-    async with temp_engine.connect() as conn:
-        await conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}"))
-        await conn.commit()
-    await temp_engine.dispose()
+    retries = 10
+    while retries > 0:
+        try:
+            async with temp_engine.connect() as conn:
+                await conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}"))
+                await conn.commit()
+            await temp_engine.dispose()
+        except (OperationalError,Exception) as e:
+            retries -= 1
+            print(f"Database not ready... waiting. Retries left: {retries} (Error: {e}")
+            if retries == 0:
+                raise e
+            await asyncio.sleep(3)
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
